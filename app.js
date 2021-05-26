1f42bf37884ce68d08a7d68af4742ce60af4dcaf
nadirbashir <1.nadirbashir@gmail.com>
2021-05-26 23:59:41 +0500

    app.js update

---
+++ app.js
@@ -0,0 +1,8 @@
+function signUp(){
+    var firstname = document.getElementById("firstName").value;
+    var lastname = document.getElementById("firstName").value;
+    var email = document.getElementById("firstName").value;
+    
+    var phone = document.getElementById("firstName").value;
+    var dob = document.getElementById("firstName").value;
+}
