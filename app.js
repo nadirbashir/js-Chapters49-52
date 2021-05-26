0d0d5ee364fad25e1df72006f8ce0710aeb5d538
nadirbashir <1.nadirbashir@gmail.com>
2021-05-27 00:56:28 +0500

    section one complete

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-function signUp(){
-    var firstname = document.getElementById("firstName").value;
-    var lastname = document.getElementById("firstName").value;
-    var email = document.getElementById("firstName").value;
-    
-    var phone = document.getElementById("firstName").value;
-    var dob = document.getElementById("firstName").value;
+function signUp() {
+  var firstname = document.getElementById("firstName").value;
+  var lastname = document.getElementById("lastName").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("phone").value;
+  var dob = document.getElementById("dob").value;
+
+  var output = document.getElementById("para");
+  output.innerHTML = "First Name: " + firstname + "<br>Last Name: " + lastname + "<br>Email: "+ email + "<br>Phone Number : " + phone + "<br>Date of birth: "+ dob;
 }
